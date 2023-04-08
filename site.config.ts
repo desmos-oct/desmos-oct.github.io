6d3d2e50a3676ea4014148fe9f8c49bd64bb4493
desmos-oct <73176121+desmos-oct@users.noreply.github.com>
2023-04-08 11:31:35 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Vermouth-s-Blog-18ef2ba4da344c558e7fdf5fbf14d296',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
